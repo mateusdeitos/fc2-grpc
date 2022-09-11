5e7a36d0f8e3cbf3fed186020d70535cfb5cb82e
Mateus Deitos <mate.deitos@hotmail.com>
2022-09-11 17:19:49 -0300

    Comentários

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -12,11 +12,15 @@ import (
 
 func main() {
 
+	// Cria o listener para o servidor na porta 50051
 	lis, err := net.Listen("tcp", "localhost:50051")
 	if err != nil {
 		log.Fatalf("Could not connect: %v", err)
 	}
 
+	log.Default().Println("Server is running...")
+
+	// inicia o servidor
 	grpcServer := grpc.NewServer()
 	pb.RegisterUserServiceServer(grpcServer, &services.UserService{})
 	reflection.Register(grpcServer)
